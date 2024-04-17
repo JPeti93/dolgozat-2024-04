90808db23c9e856d2ba56aff87fb2ca403c6ecee
JPeti93 <148367559+JPeti93@users.noreply.github.com>
2024-04-17 19:55:23 +0200

    Add files via upload

---
+++ feladat2/src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container">
+      {<p>A Balaton környékén az utóbbi időben gombamódra szaporodnak az épített balatoni kilátók, amelyeket ha megmászunk, csodás kilátás nyílik a tóra és a part menti térségre.
+A kilátók többsége csak gyalog közelíthető meg, de szinte mindegyikhez feltolhatjuk a bringánkat.
+Ezeken túl természetesen rengeteg olyan hely is található, mely kilátó nélkül is nagyszerű panorámát kínál.
+</p>}
+    </div>
+  );
+}
+
+export default Home;
